225f3b32d213cd9418d157901efd15ce649b1900
Andrei Stafie <andreistafie@gmail.com>
2025-10-09 13:13:34 +0300

    Create the register form

---
+++ register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title></title>
+</head>
+<body>
+	<form action="register.php" method="POST">
+		<input type="text" name="reg_fname" placeholder="First Name" required>
+		<br>
+		<input type="text" name="reg_lname" placeholder="Last Name" required>
+		<br>
+		<input type="email" name="email" placeholder="Email" required>
+		<br>
+		<input type="email" name="email2" placeholder="Confirm Email" required>
+		<br>
+		<input type="password" name="reg_password" placeholder="Password" required>
+		<br>
+		<input type="password" name="reg_password2" placeholder="Confirm Password"
+		 required>
+		<br>
+		<input type="button" name="reg_button" value="Register">
+	</form>
+
+</body>
+</html>
